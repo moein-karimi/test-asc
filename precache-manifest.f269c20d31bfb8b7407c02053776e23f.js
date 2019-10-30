ca83736ae6d157efd009a8467651e3ae650a28da
moein-karimi <313mtb@gmail.com>
2019-10-30 11:00:35 +0330

    Updates

---
+++ precache-manifest.f269c20d31bfb8b7407c02053776e23f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c09f8ff39efb0841a58dfe4ccc7d1c",
+    "url": "/test-asc/index.html"
+  },
+  {
+    "revision": "02be30177aa3666c614f",
+    "url": "/test-asc/static/css/main.4010e374.chunk.css"
+  },
+  {
+    "revision": "ad6207f662aa2eecbe0c",
+    "url": "/test-asc/static/js/2.930cb17e.chunk.js"
+  },
+  {
+    "revision": "02be30177aa3666c614f",
+    "url": "/test-asc/static/js/main.8d20d6d5.chunk.js"
+  },
+  {
+    "revision": "76a77f974308efd7e785",
+    "url": "/test-asc/static/js/runtime-main.6a401e5a.js"
+  }
+]);
